ddb79f883c1d9c3b8fe7e45821523b768ba15fe8
Sudheer Medepalli <smedepalli@MacBook-Pro-2.local>
2026-05-05 17:00:29 +0530

    PX installation scripts placed

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -35,6 +35,26 @@ export const AuthProvider = ({ children }) => {
     setUser(user);
     setIsAdmin(isAdminEmail(user.email));
 
+    //passing user and account objects:
+    aptrinsic("identify",
+      {
+      //User Fields
+        "id": "unique-user-id", // Required for logged in app users
+        "email": "userEmail@address.com",
+        "firstName": "John",
+        "lastName": "Smith",
+        "signUpDate": 1522697426479, //unix time in ms
+        "plan" : "gold", //Custom attributes - please create those custom attributes in Aptrinsic via Account Settings to be tracked.
+        "price" : 95.5,
+        "userHash": "" // optional transient for HMAC identification
+      },
+      {
+      //Account Fields
+        "id":"IBM", //Required
+        "name":"International Business Machine",
+        "Program": "Platinum" // flat custom attributes
+    });
+
     return { success: true };
   };
 
