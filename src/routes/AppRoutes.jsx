eb32e3094f1b0d5c1fc06ee46bef2600062d8087
Sudheer Medepalli <smedepalli@MacBook-Pro-2.local>
2026-05-05 11:17:54 +0530

    Routing issue fixed

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,11 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
-// Layout Components
 import MainLayout from "../layout/MainLayout";
 import AdminLayout from "../layout/AdminLayout";
 
-// User Application Pages
 import Landing from "../pages/Landing";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
@@ -16,22 +14,15 @@ import News from "../pages/News";
 import Forum from "../pages/Forum";
 import WidgetPuzzle from "../pages/WidgetPuzzle";
 
-// Admin Application Pages
 import Dashboard from "../admin/Dashboard";
 
 function AppRoutes() {
-  // Grab the isAdmin flag from our AuthContext to protect admin routes
   const { isAdmin } = useAuth();
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/tradingtom/">
       <Routes>
 
-        {/* 
-            USER APP ROUTES
-            All these routes are wrapped in the MainLayout component.
-            MainLayout includes the Navbar, SearchBar, and Footer. 
-        */}
         <Route element={<MainLayout />}>
           <Route path="/" element={<Landing />} />
           <Route path="/login" element={<Login />} />
@@ -44,17 +35,10 @@ function AppRoutes() {
           <Route path="/widget-puzzle" element={<WidgetPuzzle />} />
         </Route>
 
-        {/* 
-            ADMIN APP ROUTES
-            This route uses a ternary operator to check if the user is an admin.
-            If isAdmin is true, it renders AdminLayout.
-            If isAdmin is false, it redirects unauthorized users to the Login page. 
-        */}
         <Route
           path="/admin"
           element={isAdmin ? <AdminLayout /> : <Login />}
         >
-          {/* Default child route for /admin */}
           <Route index element={<Dashboard />} />
         </Route>
 
